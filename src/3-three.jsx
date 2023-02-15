ae97ed1621932843ba16dda01e0d88dc1cb6fe95
ZiyuZoda <109115725+ZiyuZoda@users.noreply.github.com>
2023-02-15 17:51:54 -0500

    Update 3-three.jsx

--- src/3-three.jsx
+++ src/3-three.jsx
@@ -1,8 +1,29 @@
 import ReactDOM from "react-dom/client";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+function Box(props) {
+  const ref = useRef();
+  useFrame((state, delta) => (ref.current.rotation.y += delta));
+  return (
+    <mesh {...props} ref={ref}>
+      <boxGeometry args={[props.length, 1, 1]} />
+      <meshStandardMaterial color={"pink"} />
+    </mesh>
+  );
+}
 
 function App() {
-  return <Canvas></Canvas>;
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      {/* ambient light */}
+      <Box length = {5} position={[-1.2, 0, 0]} />
+      <Box length = {2} position={[1.2, 0, 0]} />
+      <Box length = {4} position={[1.2, 2, 0]} />
+    </Canvas>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
